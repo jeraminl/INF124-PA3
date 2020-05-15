f5c6e180c70ce01301657016d408a0b8b2fdbaaa
jeraminl <jeraminl@uci.edu>
2020-05-14 17:42:32 -0700

    code clean up

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -73,39 +73,4 @@ function fillInfo(){
   xmlhttp.open("GET", "api/fullCatalog",true);
   console.log("sending");
   xmlhttp.send();
-
-  /*
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function (){
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById('productListOffice').innerHTML = this.responseText;
-    }
-  };
-  xmlhttp.open("GET", "./php/get_product_office.php",true);
-  console.log("sending");
-  xmlhttp.send();
-
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function (){
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById('productListGaming').innerHTML = this.responseText;
-    }
-  };
-  xmlhttp.open("GET", "./php/get_product_gaming.php",true);
-  console.log("sending");
-  xmlhttp.send();
-
-
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function (){
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById('productListProfessional').innerHTML = this.responseText;
-    }
-  };
-  xmlhttp.open("GET", "./php/get_product_professional.php",true);
-  console.log("sending");
-  xmlhttp.send();
-
-
-   */
 }
