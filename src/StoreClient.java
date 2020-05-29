1f92beef1bfa5e4429c40ab27fd903b4b7f45210
jeraminl <jeraminl@uci.edu>
2020-05-29 09:37:14 -0700

    add store client file

---
+++ src/StoreClient.java
@@ -0,0 +1,5 @@
+public class StoreClient {
+  public static void main(String[] argv) {
+    // Please, do not remove this line from file template, here invocation of web service will be inserted  
+  }
+}
